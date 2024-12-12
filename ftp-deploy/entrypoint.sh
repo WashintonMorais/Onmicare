6868ae9d34d49e023884fc5a04cc052f26f2d367
Washinton Morais <wmorais230287@gmail.com>
2024-12-12 16:34:42 -0300

    Corrigir caminho relativo para a ação personalizada

--- ftp-deploy/entrypoint.sh
+++ ftp-deploy/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh -l
 
-# Verifica se as variáveis necessárias estão presentes
-if [ -z "$INPUT_HOST" ] || [ -z "$INPUT_USER" ] || [ -z "$INPUT_PASSWORD" ]; then
-  echo "Error: Missing FTP credentials"
-  exit 1
-fi
+# ... (resto do script)
 
-# Executa o comando lftp com as variáveis de ambiente passadas
-lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ftp:ssl-force $INPUT_FORCESSL; set ssl:verify-certificate false; mirror $INPUT_OPTIONS --reverse --continue --dereference -x ^\.git/$ $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
+echo "Connecting to FTP server..."
+lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "
+    set ftp:ssl-force $INPUT_FORCESSL;
+    set ssl:verify-certificate false;
+    set debug ftp;
+    mirror $INPUT_OPTIONS --reverse --continue --dereference -x ^\.git/$ $INPUT_LOCALDIR $INPUT_REMOTEDIR;
+    quit"
